6c4ee7a6cb726a495d56030e91b48484724e6a86
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2023-09-10 19:22:20 -0300

    Create Main.java

---
+++ LAB05/Main.java
@@ -0,0 +1,23 @@
+class Main {
+  static public void main( String args[] ) {
+    System.out.println( "Hello World!" );
+    System.out.println("Olá mundE");
+    System.out.println("Engenharia Economica para Software");
+
+    int a=5;
+    int b=10;
+
+    int soma=a+b;
+
+    System.out.println(soma);
+    //teste comentário
+
+    /*
+     * teste comentários
+     * comentários
+     */
+
+
+  }
+}
+ 
